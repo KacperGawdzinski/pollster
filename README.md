b39e9759c1ff5f637a6cdb0c34ddd362354430e9
KacperGawdzinski <58347130+KacperGawdzinski@users.noreply.github.com>
2020-10-24 17:48:20 +0200

    v1.2

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Pollster
-Pollster is a simple discord bot for polls
-![](https://ibb.co/607YrRG)
+[Pollster](https://top.gg/bot/769122743786143754) is a simple discord bot for polls<br><br>
+![](https://i.ibb.co/6PKSKYx/Pollster-screen.png)
 ## Usage
- - [ ] Every command starts with prefix '!'
- - [ ] [!poll question; answer1; answer2; ...] creates a poll; by default it lasts for an hour however you can change the time by adding a number with suffix [s, m, h, d, w] as last argument for answers (for second, minutec etc.)
- - [ ] [!close] - closes opened poll
- - [ ] [!info] - shows manual
- - [ ] [!version] - shows version
+ * Every command starts with prefix '!'
+
+| Command        				| Description         				
+| ------------- 				|:-------------				
+| !poll question; answer1; answer2; ...		| Creates new poll which lasts for five minutes 
+| !poll question; answer1; answer2; ... ; 5h    | Creates new poll with custom expiration time [s, m, h, d, w] (seconds, minutes, etc.)   				
+| !info		 				| Shows information about bot			
+| !version	 				| Shows version			
--- pollster.js
+++ pollster.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js')
+const { version } = require('./package.json');
+const { prefix } = require('./package.json');
 const client = new Discord.Client()
-const prefix = '!'
-const version = "1.0"
 const Number = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣','9️⃣']
-let timer = 0
-let poll = false 
+
 
 client.on('ready', () => {
     console.log('Pollster is online')
@@ -15,21 +14,34 @@ client.on('message', msg => {
     let temp = msg.content.slice(prefix.length)
     let args = temp.endsWith(';') ? temp.slice(0, temp.length - 1).split(';') : temp.split(';')
     args = args.map(arg => arg.trim())
-    let t = /^\d[smhdw]$/
+    let t = /^\d+[smhdw]$/
     let time = 0
     if(t.test(args[args.length - 1])) {
-        time = args[args.length - 1].replace(/^.[smhdw]$/,'')   //TODO - time
+        let char = args[args.length - 1].slice(-1)
+        switch(char) {
+            case 's':
+                time = args[args.length - 1].slice(0, -1) * 1000
+            break
+            case 'm':
+                time = args[args.length - 1].slice(0, -1) * 60000
+            break
+            case 'h':
+                time = args[args.length - 1].slice(0, -1) * 3600000
+            break
+            case 'd':
+                time = args[args.length - 1].slice(0, -1) * 84400000
+            break
+            case 'w':
+                time = args[args.length - 1].slice(0, -1) * 604800000
+            break
+        }
         args.length -= 1
     } else {
-        time = 60000
+        time = 60000 * 5
     }
     let arg_0_splitted = args[0].split(' ')
     switch(arg_0_splitted[0]) {
         case 'poll':
-            if(poll == true) {
-                msg.channel.send("```One poll already runs! Close it and try again```")
-                return
-            }
             if(arg_0_splitted.length == 1 || args.length < 3) {
                 msg.channel.send("```Insert question and at least 2 answers!```")
                 return
@@ -50,17 +62,14 @@ client.on('message', msg => {
                 for(let i = 1; i < args.length; i++) {
                     sentEmbed.react(Number[i - 1])
                 }
-                msg_id = sentEmbed.id
-            })
-            
-            timer = setTimeout(function() {
-                msg.channel.messages.fetch({around: msg_id, limit: 1})
-                .then(messages => {
-                    let x = messages.first().reactions.cache.array()
+                let filter = (reaction) => Number.includes(reaction.emoji.name)
+                sentEmbed.awaitReactions(filter, { time: time })
+                .then(collected => {
+                    let coll = collected.array()
                     let max = 0, k = 0
-                    for(let i = 0; i < x.length; i++) {
-                        if(x[i].count > max) {
-                            max = x[i].count
+                    for(let i = 0; i < coll.length; i++) {
+                        if(coll[i].count > max) {
+                            max = coll[i].count
                             k = i
                         }
                     }
@@ -70,24 +79,26 @@ client.on('message', msg => {
                     .addField('Winner is answer ' + Number[k] + ' : ' + args[k + 1] + '!', 'Congratulations!')
                     msg.channel.send(embed)
                 })
-            }, time);
+            })
         break;
-        case 'close':
-            poll = false
-            if(timer === 0) {
-                msg.channel.send('Any survey runs now!')
-                return
-            }
-            clearTimeout(timer);
-            msg.channel.send('Survey canceled')
+        case 'info':
+            let embed2 = new Discord.MessageEmbed()
+            .setTitle('Usage information')
+            .setColor('#F93A2F')
+            .addField('!poll question; answer1; answer2; ...', 'Creates new poll which lasts for five minutes')
+            .addField('!poll question; answer1; answer2; ... ; 5h', 'Creates new poll with custom expiration time [s, m, h, d, w] (seconds, minutes, etc.)')
+            .addField('!info','Shows this page')
+            .addField('!version','Shows version')
+            .setFooter('by @Grzanol')
+            msg.channel.send(embed2)
         break
         case 'version':
-            let embed2 = new Discord.MessageEmbed()
+            let embed3 = new Discord.MessageEmbed()
             .setTitle('Version: ' + version)
             .setColor('#F93A2F')
-            msg.channel.send(embed2)
+            msg.channel.send(embed3)
         break
     }
 })
 
-client.login('NzY5MTIyNzQzNzg2MTQzNzU0.X5KbXw.wgrzBLPDiyRurvDqUXpjizZBJvk')
+client.login(process.env.token)
